f7090010110fa8f6c32dfd9fbe44ec1e76375340
Altay Hodoglugil <altay.hodo@gmail.com>
2024-10-16 21:45:18 -0700

    Add gitignore, fix frontend skeleton

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+
+  return (
+    <>
+      <div>ResumAI</div>
+    </>
+  )
+}
+
+export default HomePage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import HomePage from './homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePage></HomePage>
   </StrictMode>,
 )
