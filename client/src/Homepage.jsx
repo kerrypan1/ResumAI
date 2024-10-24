74eeb2af46953d60e47dfc4ee0abe13bd6441325
Cathryn Chang <cathrync4735@gmail.com>
2024-10-23 19:56:07 -0700

    Created upload resume button

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,7 +1,16 @@
+
+
 function HomePage() {
+  function handleChange(event){
+    setFile(event.target.files[0])
+  }
   return (
     <>
       <div>ResumAI</div>
+      <form>
+        <input type="file" onChange={handleChange}/>
+      </form>
+      
     </>
   )
 }
