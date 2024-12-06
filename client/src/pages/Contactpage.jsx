7f677e54d3cf9f3cd790d67d2dfbd9e6ca09240e
Katy Y <katyyang0@gmail.com>
2024-12-05 19:58:20 -0800

    edited contact page

--- client/src/pages/Contactpage.jsx
+++ client/src/pages/Contactpage.jsx
@@ -1,7 +1,221 @@
+import React from 'react';
+import '../styles/ContactPage.css';
+
 function ContactPage() {
     return (
       <>
-        <h1>ContactPage</h1>
+        <div className="card-container">
+          <div className="section-card animate-fadeIn">
+            {/* First Profile Set */}
+            <h2 className="section-title">Project Leads</h2>
+
+            <div className="profile-container">
+              {/* First Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Eric Liang</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+              {/* Second Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+              <div className='description-text'>
+                <h3>Kerry Pan</h3> 
+                <p>email</p>
+              </div>
+            </div>
+          </div> 
+        </div>
+
+        <div className="section-card animate-fadeIn">
+            {/* First Profile Set */}
+            <h2 className="section-title">Frontend</h2>
+                        <div className="profile-container">
+              {/* First Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Vivian Shen</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+              {/* Second Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+              <div className='description-text'>
+                <h3>Cathryn Chang</h3> 
+                <p>email</p>
+              </div>
+            </div>
+              {/* Third Profile */}
+                          <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Katy Yang</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+              
+          </div>
+        </div>
+
+        <div className="section-card animate-fadeIn">
+            {/* First Profile Set */}
+            <h2 className="section-title">Backend</h2>
+                        <div className="profile-container">
+              {/* First Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Altay Hodoglugil</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+              {/* Second Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Arthur Laureus Wigo</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+               {/* Third Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                  </div>
+                <div className='description-text'>
+                  <h3>Jesslyn Ashley Ruswan</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+                 {/* Fourth Profile */}
+                 <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Catherine Zhuang</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        <div className="section-card animate-fadeIn">
+            {/* First Profile Set */}
+            <h2 className="section-title">Data Science</h2>
+                        <div className="profile-container">
+              {/* First Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>William Chang</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+              {/* Second Profile */}
+              <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+              <div className='description-text'>
+                <h3>Daniel Li</h3> 
+                <p>email</p>
+              </div>
+            </div>
+
+                          {/* Second Profile */}
+                          <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Michelle Li</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+                            {/* Second Profile */}
+                            <div className="profile-item">
+                <div className="profile-circle">
+                  <img 
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                    className="profile-image" 
+                  />
+                </div>
+                <div className='description-text'>
+                  <h3>Yeonju Seo</h3> 
+                  <p>email</p>
+                </div>
+              </div>
+
+          </div>
+        </div>
+      </div>
+      
+      
+
+
       </>
     );
   }
